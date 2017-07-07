699a7b67998e8a6633e1cc78156cfdd99b97379d
LSune <1653673717@qq.com>
2017-07-07 13:20:29 +0800

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.  On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	new file:   Pictures/fetal.png
    	new file:   Pictures/perfect.png
    	modified:   app/build.gradle
    	modified:   app/src/main/AndroidManifest.xml
    	new file:   app/src/main/ic_launcher-web.png
    	modified:   app/src/main/java/com/example/unreallover/activitytest/FirstActivity.java
    	new file:   app/src/main/res/drawable/apple_icon.png
    	new file:   app/src/main/res/drawable/ic_list.png
    	new file:   app/src/main/res/drawable/ic_random.png
    	new file:   app/src/main/res/drawable/ice_princess.jpg
    	new file:   app/src/main/res/drawable/little_green.jpg
    	new file:   app/src/main/res/drawable/nav_cache.png
    	new file:   app/src/main/res/drawable/nav_collection.png
    	new file:   app/src/main/res/drawable/nav_suggest.png
    	modified:   app/src/main/res/layout/first_layout.xml
    	new file:   app/src/main/res/layout/nav_header.xml
    	new file:   app/src/main/res/menu/nav_menu.xml
    	modified:   app/src/main/res/mipmap-hdpi/ic_launcher.png
    	modified:   app/src/main/res/mipmap-mdpi/ic_launcher.png
    	modified:   app/src/main/res/mipmap-xhdpi/ic_launcher.png
    	modified:   app/src/main/res/mipmap-xxhdpi/ic_launcher.png
    	modified:   app/src/main/res/mipmap-xxxhdpi/ic_launcher.png
    	modified:   app/src/main/res/values/colors.xml

--- app/src/main/java/com/example/unreallover/activitytest/FirstActivity.java
+++ app/src/main/java/com/example/unreallover/activitytest/FirstActivity.java
@@ -1,15 +1,28 @@
 package com.example.unreallover.activitytest;
 
+import android.content.Context;
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.view.View.OnClickListener;
+import com.bumptech.glide.Glide;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -17,10 +30,14 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.List;
 
-public class FirstActivity extends AppCompatActivity implements View.OnClickListener
+public class FirstActivity extends AppCompatActivity// implements View.OnClickListener  不屏蔽这一句就会出现Error:(21, 8) 错误: FirstActivity不是抽象的, 并且未覆盖OnClickListener中的抽象方法onClick(View)
 {
     TextView responseText;
+    TextView responseTitle;
+    TextView responseAuthor;
+    private DrawerLayout mDrawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -28,25 +45,52 @@ public class FirstActivity extends AppCompatActivity implements View.OnClickList
         setContentView(R.layout.first_layout);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        Button sendRequest = (Button) findViewById(R.id.send_request);
-        responseText = (TextView) findViewById(R.id.response_text);
-        sendRequest.setOnClickListener(this);
-    }
-    @Override
-    public void onClick(View v) {
-        if (v.getId() == R.id.send_request) {
-            sendRequestWithHttpURLConnection();
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_list);
+
         }
+        responseTitle = (TextView) findViewById(R.id.response_title);
+        responseAuthor = (TextView) findViewById(R.id.response_author);
+        responseText = (TextView) findViewById(R.id.response_text);
+        sendRequestWithHttpURLConnection();
+        FloatingActionButton random = (FloatingActionButton)
+                findViewById(R.id.random);
+        random.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                if (v.getId() == R.id.random) {
+                    sendRequestWithHttpURLConnection();
+                }
+                Toast.makeText(FirstActivity.this, "正在随机……", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+       /* FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.random);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FirstActivity.this, "FAB clicked", Toast.LENGTH_
+                        SHORT).show();
+            }
+        });*/
     }
+
+    private static final String TAG = "FirstActivity";
     private void sendRequestWithHttpURLConnection() {
+        Log.e(TAG, "sendRequestWithHttpURLConnection: " );
 // 开启线程来发起网络请求
         new Thread(new Runnable() {
             @Override
             public void run() {
+                Log.e(TAG, "run: "+Thread.currentThread() );
                 HttpURLConnection connection = null;
                 BufferedReader reader = null;
                 try {
-                    URL url = new URL("https://www.baidu.com");
+                    URL url = new URL("https://meiriyiwen.com/random/iphone");
                     connection = (HttpURLConnection) url.openConnection();
                     connection.setRequestMethod("GET");
                     connection.setConnectTimeout(8000);
@@ -77,15 +121,34 @@ public class FirstActivity extends AppCompatActivity implements View.OnClickList
             }
         }).start();
     }
-    private void showResponse(final String response) {
+
+    public void showResponse(final String response) {
+        /*开始提取标题*/
+        int t1=response.indexOf("title",0);
+        int t2=response.indexOf("-", t1);
+        int t3=response.indexOf("-", t2+1);
+        int t4=response.indexOf("<p>", t3);
+        int t5=response.lastIndexOf("</p>");//括号里再加上  ，t4  会有神奇的事情发生
+        final String title1=response.substring(t1+6,t2);
+        final String title=title1+"";
+        final String author1=response.substring(t2+1,t3);
+        final String author=" \n"+author1+" \n";
+        final String text3=response.substring(t4+3,t5);
+        final String text2=text3.replaceAll("<p>","");
+        final String text1=text2.replaceAll("</p>","\n\n        ");
+        final String text="        "+text1;
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
 // 在这里进行 UI 操作，将结果显示到界面上
-                responseText.setText(response);
+                responseText.setText(text);
+                responseAuthor.setText(author);
+                responseTitle.setText(title);
             }
         });
     }
+
+
 
     public boolean onCreateOptionsMenu(Menu menu)
     {
@@ -97,6 +160,9 @@ public class FirstActivity extends AppCompatActivity implements View.OnClickList
     {
         switch (item.getItemId())
         {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                break;
             case R.id.collect:
                 Toast.makeText(this, "已收藏", Toast.LENGTH_SHORT).
                         show();
